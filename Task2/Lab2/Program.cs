6b1f570421b126f280cc8a0d5012c68ab34eb441
DmitryGil <91392061+dmitrygil1@users.noreply.github.com>
2022-02-07 22:05:38 +0300

    Add files via upload

---
+++ Task2/Lab2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2
+{
+    class Program
+    {
+        // Статическая типизация (место под прогр. выделяется при кампиляции)
+        static int f(int x, int y)
+        {
+            x++;
+            y -= 2;
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            int x = 5, y = 6;
+            Console.WriteLine(f(x, y) + " x=" + x + " y=" + y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double F;
+            Console.WriteLine("Введите х");
+            // Преобразуем строковое значение в целочисленное
+            x = int.Parse(Console.ReadLine());
+
+            if (x < 0)
+                F = 0;
+            else if (x >= 0 && x <= 1)
+                F = 1 / (Math.Pow(x, 2) + 1);
+            else if (x > 1 && x <= 4)
+                F = Math.Pow(x, 3);
+            else
+                F = 64 + Math.Log (x,8);
+            Console.WriteLine(" F =" +F );
+            Console.ReadKey();
+        }
+    }
+}
